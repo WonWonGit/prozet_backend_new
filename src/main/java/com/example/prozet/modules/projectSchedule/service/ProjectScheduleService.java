9c92036724f78958ca7ee0e1194619f15427afe1
WonWonGit <bkn020612@gmail.com>
2023-02-28 21:34:59 +0900

    project schedule edit service test

--- src/main/java/com/example/prozet/modules/projectSchedule/service/ProjectScheduleService.java
+++ src/main/java/com/example/prozet/modules/projectSchedule/service/ProjectScheduleService.java
@@ -2,7 +2,6 @@ package com.example.prozet.modules.projectSchedule.service;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,121 +15,124 @@ import com.example.prozet.modules.project.domain.dto.response.ProjectResDTO;
 import com.example.prozet.modules.projectMember.domain.dto.response.ProjectMemberResDTO;
 import com.example.prozet.modules.projectMember.domain.entity.ProjectMemberEntity;
 import com.example.prozet.modules.projectMember.repository.ProjectMemberRepository;
-import com.example.prozet.modules.projectSchedule.domain.dto.request.ProjectScheduleEditReqDTO;
 import com.example.prozet.modules.projectSchedule.domain.dto.request.ProjectScheduleSaveReqDTO;
 import com.example.prozet.modules.projectSchedule.domain.dto.response.ProjectScheduleResDTO;
 import com.example.prozet.modules.projectSchedule.domain.entity.ProjectScheduleEntity;
 import com.example.prozet.modules.projectSchedule.repository.ProjectScheduleRepository;
 import com.example.prozet.modules.schedule.domain.dto.response.ScheduleResDTO;
-import com.example.prozet.modules.schedule.domain.entity.ScheduleEntity;
 import com.example.prozet.modules.schedule.repository.ScheduleRepository;
 
 @Service
 @Transactional(readOnly = true)
 public class ProjectScheduleService {
 
-    @Autowired
-    private ProjectScheduleRepository projectScheduleRepository;
+        @Autowired
+        private ProjectScheduleRepository projectScheduleRepository;
 
-    @Autowired
-    private ScheduleRepository scheduleRepository;
+        @Autowired
+        private ScheduleRepository scheduleRepository;
 
-    @Autowired
-    private ProjectMemberRepository projectMemberRepository;
+        @Autowired
+        private ProjectMemberRepository projectMemberRepository;
 
-    @Transactional
-    public ProjectScheduleResDTO saveProjectSchedule(ProjectResDTO projectResDTO,
-            ProjectScheduleSaveReqDTO projectScheduleSaveReqDTO,
-            ProjectMemberResDTO projectMemberResDTO) {
+        @Transactional
+        public ProjectScheduleResDTO saveProjectSchedule(ProjectResDTO projectResDTO,
+                        ProjectScheduleSaveReqDTO projectScheduleSaveReqDTO,
+                        ProjectMemberResDTO projectMemberResDTO) {
 
-        ProjectScheduleEntity projectScheduleEntity = ProjectScheduleEntity.builder()
-                .projectEntity(projectResDTO.toEntity())
-                .scheduleEntity(projectScheduleSaveReqDTO.getScheduleReqDTO().toEntity())
-                .projectMemberEntity(projectMemberResDTO.toEntity())
-                .scheduleType(projectScheduleSaveReqDTO.getScheduleType())
-                .build();
+                ProjectScheduleEntity projectScheduleEntity = ProjectScheduleEntity.builder()
+                                .projectEntity(projectResDTO.toEntity())
+                                .scheduleEntity(projectScheduleSaveReqDTO.getScheduleReqDTO().toEntity())
+                                .projectMemberEntity(projectMemberResDTO.toEntity())
+                                .scheduleType(projectScheduleSaveReqDTO.getScheduleType())
+                                .build();
 
-        ProjectScheduleEntity projectScheduleEntityPS = projectScheduleRepository.save(projectScheduleEntity);
+                ProjectScheduleEntity projectScheduleEntityPS = projectScheduleRepository.save(projectScheduleEntity);
 
-        if (projectScheduleEntityPS != null) {
-            return projectScheduleEntityPS.toProjectScheduleResDTO();
+                if (projectScheduleEntityPS != null) {
+                        return projectScheduleEntityPS.toProjectScheduleResDTO();
+                }
+
+                return null;
         }
 
-        return null;
-    }
+        @Transactional
+        public Map<Long, List<ProjectScheduleResDTO>> editProjectScheduleType(Long idx,
+                        ScheduleType scheduleType) {
 
-    @Transactional
-    public Map<ScheduleResDTO, List<ProjectScheduleResDTO>> editProjectScheduleType(Long idx,
-            ScheduleType scheduleType) {
+                List<ProjectScheduleEntity> projectScheduleEntity = projectScheduleRepository
+                                .findByScheduleEntity_Idx(idx);
 
-        List<ProjectScheduleEntity> projectScheduleEntity = projectScheduleRepository.findByScheduleEntity_Idx(idx);
+                projectScheduleEntity.forEach((projectSchedule) -> {
+                        projectSchedule.editProjectScheduleType(scheduleType);
+                });
 
-        projectScheduleEntity.forEach((projectSchedule) -> {
-            projectSchedule.editProjectScheduleType(scheduleType);
-        });
+                List<ProjectScheduleResDTO> projectScheduleResDTOs = projectScheduleEntity.stream()
+                                .map(ProjectScheduleEntity::toProjectScheduleResDTO).collect(Collectors.toList());
 
-        List<ProjectScheduleResDTO> projectScheduleResDTOs = projectScheduleEntity.stream()
-                .map(ProjectScheduleEntity::toProjectScheduleResDTO).collect(Collectors.toList());
+                Map<Long, List<ProjectScheduleResDTO>> projectSchduleResDTO = projectScheduleResDTOs.stream()
+                                .collect(Collectors.groupingBy(projectScheduleResDTO -> projectScheduleResDTO
+                                                .getScheduleResDTO().getIdx()));
 
-        Map<ScheduleResDTO, List<ProjectScheduleResDTO>> projectSchduleResDTO = projectScheduleResDTOs.stream()
-                .collect(Collectors.groupingBy(ProjectScheduleResDTO::getScheduleResDTO));
+                return projectSchduleResDTO;
 
-        return projectSchduleResDTO;
-
-    }
-
-    @Transactional
-    public void editProjectScheudleDeleteMember(Long scheduleIdx, Long projectMemberIdx) {
-
-        ProjectScheduleEntity projectScheduleEntity = projectScheduleRepository
-                .findByScheduleEntity_IdxAndProjectMemberEntity_Idx(scheduleIdx, projectMemberIdx)
-                .orElseThrow(() -> new CustomException(ErrorCode.PROJECT_SCHEDULE_NOT_EXIST));
-
-        projectScheduleRepository.delete(projectScheduleEntity);
-
-    }
-
-    @Transactional
-    public ProjectScheduleResDTO editProjectScheudleAddMember(Long scheduleIdx, Long projectMemberIdx) {
-
-        List<ProjectScheduleEntity> projectScheduleEntity = projectScheduleRepository
-                .findByScheduleEntity_Idx(scheduleIdx);
-
-        boolean memberCheck = projectScheduleEntity.stream()
-                .anyMatch(projectSchedule -> projectSchedule.getProjectMemberEntity().getIdx() == projectMemberIdx);
-
-        if (!memberCheck) {
-
-            ProjectMemberEntity projectMemberEntity = projectMemberRepository.findByIdx(projectMemberIdx)
-                    .orElseThrow(() -> new CustomException(ErrorCode.PROJECT_MEMBER_NOT_EXIST));
-
-            ProjectScheduleEntity projectScheduleEntity2 = ProjectScheduleEntity.builder()
-                    .scheduleEntity(projectScheduleEntity.get(0).getScheduleEntity())
-                    .projectMemberEntity(projectMemberEntity)
-                    .projectEntity(projectScheduleEntity.get(0).getProjectEntity())
-                    .scheduleType(projectScheduleEntity.get(0).getScheduleType())
-                    .build();
-
-            ProjectScheduleEntity projectScheduleEntityPS = projectScheduleRepository.save(projectScheduleEntity2);
-
-            return projectScheduleEntityPS.toProjectScheduleResDTO();
         }
 
-        return null;
+        @Transactional
+        public void editProjectScheudleDeleteMember(Long scheduleIdx, Long projectMemberIdx) {
 
-    }
+                ProjectScheduleEntity projectScheduleEntity = projectScheduleRepository
+                                .findByScheduleEntity_IdxAndProjectMemberEntity_Idx(scheduleIdx, projectMemberIdx)
+                                .orElseThrow(() -> new CustomException(ErrorCode.PROJECT_SCHEDULE_NOT_EXIST));
 
-    public Map<ScheduleResDTO, List<ProjectScheduleResDTO>> findProjectSchedule(Long idx) {
+                projectScheduleRepository.delete(projectScheduleEntity);
 
-        List<ProjectScheduleEntity> projectScheduleEntity = projectScheduleRepository.findByScheduleEntity_Idx(idx);
+        }
 
-        List<ProjectScheduleResDTO> projectScheduleResDTOs = projectScheduleEntity.stream()
-                .map(ProjectScheduleEntity::toProjectScheduleResDTO).collect(Collectors.toList());
+        @Transactional
+        public ProjectScheduleResDTO editProjectScheudleAddMember(Long scheduleIdx, Long projectMemberIdx) {
 
-        Map<ScheduleResDTO, List<ProjectScheduleResDTO>> projectSchduleResDTO = projectScheduleResDTOs.stream()
-                .collect(Collectors.groupingBy(ProjectScheduleResDTO::getScheduleResDTO));
+                List<ProjectScheduleEntity> projectScheduleEntity = projectScheduleRepository
+                                .findByScheduleEntity_Idx(scheduleIdx);
 
-        return projectSchduleResDTO;
-    }
+                boolean memberCheck = projectScheduleEntity.stream()
+                                .anyMatch(projectSchedule -> projectSchedule.getProjectMemberEntity()
+                                                .getIdx() == projectMemberIdx);
+
+                if (!memberCheck) {
+
+                        ProjectMemberEntity projectMemberEntity = projectMemberRepository.findByIdx(projectMemberIdx)
+                                        .orElseThrow(() -> new CustomException(ErrorCode.PROJECT_MEMBER_NOT_EXIST));
+
+                        ProjectScheduleEntity projectScheduleEntity2 = ProjectScheduleEntity.builder()
+                                        .scheduleEntity(projectScheduleEntity.get(0).getScheduleEntity())
+                                        .projectMemberEntity(projectMemberEntity)
+                                        .projectEntity(projectScheduleEntity.get(0).getProjectEntity())
+                                        .scheduleType(projectScheduleEntity.get(0).getScheduleType())
+                                        .build();
+
+                        ProjectScheduleEntity projectScheduleEntityPS = projectScheduleRepository
+                                        .save(projectScheduleEntity2);
+
+                        return projectScheduleEntityPS.toProjectScheduleResDTO();
+                }
+
+                return null;
+
+        }
+
+        public Map<ScheduleResDTO, List<ProjectScheduleResDTO>> findProjectSchedule(Long idx) {
+
+                List<ProjectScheduleEntity> projectScheduleEntity = projectScheduleRepository
+                                .findByScheduleEntity_Idx(idx);
+
+                List<ProjectScheduleResDTO> projectScheduleResDTOs = projectScheduleEntity.stream()
+                                .map(ProjectScheduleEntity::toProjectScheduleResDTO).collect(Collectors.toList());
+
+                Map<ScheduleResDTO, List<ProjectScheduleResDTO>> projectSchduleResDTO = projectScheduleResDTOs.stream()
+                                .collect(Collectors.groupingBy(ProjectScheduleResDTO::getScheduleResDTO));
+
+                return projectSchduleResDTO;
+        }
 
 }
--- src/test/java/com/example/prozet/module/projectSchedule/service/ProjectScheduleServiceTest.java
+++ src/test/java/com/example/prozet/module/projectSchedule/service/ProjectScheduleServiceTest.java
@@ -1,12 +1,14 @@
 package com.example.prozet.module.projectSchedule.service;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -28,95 +30,154 @@ import com.example.prozet.modules.projectSchedule.domain.entity.ProjectScheduleE
 import com.example.prozet.modules.projectSchedule.repository.ProjectScheduleRepository;
 import com.example.prozet.modules.projectSchedule.service.ProjectScheduleService;
 import com.example.prozet.modules.schedule.domain.dto.request.ScheduleReqDTO;
+import com.example.prozet.modules.schedule.domain.dto.response.ScheduleResDTO;
 import com.example.prozet.modules.schedule.domain.entity.ScheduleEntity;
 
 @ExtendWith(MockitoExtension.class)
 public class ProjectScheduleServiceTest {
 
-    @InjectMocks
-    private ProjectScheduleService projectScheduleService;
+        @InjectMocks
+        private ProjectScheduleService projectScheduleService;
 
-    @Mock
-    private ProjectScheduleRepository projectScheduleRepository;
+        @Mock
+        private ProjectScheduleRepository projectScheduleRepository;
 
-    @Test
-    public void saveProjectScheduleTest() {
+        @Test
+        public void saveProjectScheduleTest() {
 
-        ProjectScheduleEntity projectScheduleEntity = getPublicScheduleEntity();
+                ProjectScheduleEntity projectScheduleEntity = getPublicScheduleEntity();
 
-        ScheduleReqDTO scheduleReqDTO = ScheduleReqDTO.builder()
-                .content("content")
-                .title("title")
-                .startDate(LocalDateTime.now())
-                .endDate(LocalDateTime.now())
-                .build();
+                ScheduleReqDTO scheduleReqDTO = ScheduleReqDTO.builder()
+                                .content("content")
+                                .title("title")
+                                .startDate(LocalDateTime.now())
+                                .endDate(LocalDateTime.now())
+                                .build();
 
-        List<Long> projectMemberIdx = new ArrayList<>();
-        projectMemberIdx.add(1L);
-        projectMemberIdx.add(2L);
+                List<Long> projectMemberIdx = new ArrayList<>();
+                projectMemberIdx.add(1L);
+                projectMemberIdx.add(2L);
 
-        ProjectScheduleSaveReqDTO projectScheduleSaveReqDTO = ProjectScheduleSaveReqDTO.builder()
-                .projectKey("projectKey")
-                .scheduleReqDTO(scheduleReqDTO)
-                .projectMemberIdx(projectMemberIdx)
-                .scheduleType(ScheduleType.OPEN)
-                .build();
+                ProjectScheduleSaveReqDTO projectScheduleSaveReqDTO = ProjectScheduleSaveReqDTO.builder()
+                                .projectKey("projectKey")
+                                .scheduleReqDTO(scheduleReqDTO)
+                                .projectMemberIdx(projectMemberIdx)
+                                .scheduleType(ScheduleType.OPEN)
+                                .build();
 
-        when(projectScheduleRepository.save(any())).thenReturn(projectScheduleEntity);
+                when(projectScheduleRepository.save(any())).thenReturn(projectScheduleEntity);
 
-        ProjectResDTO projectResDTO = ProjectResDTO.builder()
-                .idx(1L)
-                .projectKey("projectKey")
-                .deleteYn("N")
-                .build();
+                ProjectResDTO projectResDTO = ProjectResDTO.builder()
+                                .idx(1L)
+                                .projectKey("projectKey")
+                                .deleteYn("N")
+                                .build();
 
-        ProjectScheduleResDTO projectScheduleResDTO = projectScheduleService.saveProjectSchedule(
-                projectResDTO,
-                projectScheduleSaveReqDTO,
-                projectScheduleEntity.getProjectMemberEntity().toProjectMemberResDTO());
+                ProjectScheduleResDTO projectScheduleResDTO = projectScheduleService.saveProjectSchedule(
+                                projectResDTO,
+                                projectScheduleSaveReqDTO,
+                                projectScheduleEntity.getProjectMemberEntity().toProjectMemberResDTO());
 
-        assertThat(projectScheduleResDTO).isNotNull();
+                assertThat(projectScheduleResDTO).isNotNull();
 
-    }
+        }
 
-    public ProjectScheduleEntity getPublicScheduleEntity() {
+        @Test
+        public void editProjectScheduleTypeTest() {
 
-        ProjectEntity projectEntity = ProjectEntity.builder()
-                .projectKey("projectKey")
-                .deleteYn("N")
-                .build();
+                List<ProjectScheduleEntity> projectScheduleEntities = new ArrayList<>();
+                projectScheduleEntities.add(getPublicScheduleEntity());
+                projectScheduleEntities.add(getPublicScheduleEntity2());
 
-        MemberEntity owner = MemberEntity.builder()
-                .username("google_1212")
-                .name("name")
-                .email("test@google.com")
-                .provider(Provider.GOOGLE)
-                .displayName("test")
-                .build();
+                when(projectScheduleRepository.findByScheduleEntity_Idx(anyLong())).thenReturn(projectScheduleEntities);
 
-        ProjectMemberEntity projectMemberEntity = ProjectMemberEntity.builder()
-                .access(AccessType.EDIT)
-                .projectEntity(projectEntity)
-                .projectMemberType(ProjectMemberType.OWNER)
-                .memberEntity(owner)
-                .build();
+                Map<Long, List<ProjectScheduleResDTO>> projectSchduleResDTO = projectScheduleService
+                                .editProjectScheduleType(1L, ScheduleType.INPROGRESS);
 
-        ScheduleEntity scheduleEntity = ScheduleEntity.builder()
-                .content("content")
-                .title("title")
-                .startDate(LocalDateTime.now())
-                .endDate(LocalDateTime.now())
-                .build();
+                System.out.println(projectSchduleResDTO + "$$$$");
 
-        ProjectScheduleEntity projectScheduleEntity = ProjectScheduleEntity.builder()
-                .scheduleType(ScheduleType.OPEN)
-                .projectMemberEntity(projectMemberEntity)
-                .projectEntity(projectEntity)
-                .scheduleEntity(scheduleEntity)
-                .build();
+        }
 
-        return projectScheduleEntity;
+        public ProjectScheduleEntity getPublicScheduleEntity() {
 
-    }
+                ProjectEntity projectEntity = ProjectEntity.builder()
+                                .projectKey("projectKey")
+                                .deleteYn("N")
+                                .build();
+
+                MemberEntity owner = MemberEntity.builder()
+                                .username("google_1212")
+                                .name("name")
+                                .email("test@google.com")
+                                .provider(Provider.GOOGLE)
+                                .displayName("test")
+                                .build();
+
+                ProjectMemberEntity projectMemberEntity = ProjectMemberEntity.builder()
+                                .access(AccessType.EDIT)
+                                .projectEntity(projectEntity)
+                                .projectMemberType(ProjectMemberType.OWNER)
+                                .memberEntity(owner)
+                                .build();
+
+                ScheduleEntity scheduleEntity = ScheduleEntity.builder()
+                                .idx(1L)
+                                .content("content")
+                                .title("title")
+                                .startDate(LocalDateTime.now())
+                                .endDate(LocalDateTime.now())
+                                .build();
+
+                ProjectScheduleEntity projectScheduleEntity = ProjectScheduleEntity.builder()
+                                .scheduleType(ScheduleType.OPEN)
+                                .projectMemberEntity(projectMemberEntity)
+                                .projectEntity(projectEntity)
+                                .scheduleEntity(scheduleEntity)
+                                .build();
+
+                return projectScheduleEntity;
+
+        }
+
+        public ProjectScheduleEntity getPublicScheduleEntity2() {
+
+                ProjectEntity projectEntity = ProjectEntity.builder()
+                                .projectKey("projectKey")
+                                .deleteYn("N")
+                                .build();
+
+                MemberEntity member = MemberEntity.builder()
+                                .username("google_121212")
+                                .name("name")
+                                .email("test@google.com")
+                                .provider(Provider.GOOGLE)
+                                .displayName("test")
+                                .build();
+
+                ProjectMemberEntity projectMemberEntity = ProjectMemberEntity.builder()
+                                .access(AccessType.EDIT)
+                                .projectEntity(projectEntity)
+                                .projectMemberType(ProjectMemberType.TEAMMEMBER)
+                                .memberEntity(member)
+                                .build();
+
+                ScheduleEntity scheduleEntity = ScheduleEntity.builder()
+                                .idx(1L)
+                                .content("content")
+                                .title("title")
+                                .startDate(LocalDateTime.now())
+                                .endDate(LocalDateTime.now())
+                                .build();
+
+                ProjectScheduleEntity projectScheduleEntity = ProjectScheduleEntity.builder()
+                                .scheduleType(ScheduleType.OPEN)
+                                .projectMemberEntity(projectMemberEntity)
+                                .projectEntity(projectEntity)
+                                .scheduleEntity(scheduleEntity)
+                                .build();
+
+                return projectScheduleEntity;
+
+        }
 
 }
